e7a9e010e6a4c4ea49b8f488c2bbabe35316d6e9
adityaks11 <90915537+adityaks11@users.noreply.github.com>
2022-06-03 21:37:55 +0530

    Add files via upload

---
+++ autoboxing.java
@@ -0,0 +1,11 @@
+//primitive to wrapper
+public class autoboxing
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        Integer i=Integer.valueOf(a);
+        Integer j=a;
+        System.out.println(a+" "+i+" "+j);
+    }
+}
