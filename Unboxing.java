8afe4080afd6cccde366b7ff3892b65e98dad181
adityaks11 <90915537+adityaks11@users.noreply.github.com>
2022-06-03 21:46:43 +0530

    Add files via upload

---
+++ Unboxing.java
@@ -0,0 +1,10 @@
+//Wrapper to Primitive
+public class Unboxing
+{
+    public static void main(String args[]){
+        Integer a=new Integer(10);
+        int i=a.intValue();
+        int j=a;
+        System.out.println(a+" "+i+" "+j);
+    }
+}
