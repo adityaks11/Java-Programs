5f3b27be6e40bfb69db2abc1e18aec8115c53145
adityaks11 <90915537+adityaks11@users.noreply.github.com>
2022-06-18 16:11:34 +0530

    Add files via upload

---
+++ Collection/addAll.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class addAll
+{
+    public static void main(String[] args)
+    {
+        List<String> val1 = new ArrayList<>();
+        val1.add("Hello");
+        List<String> val2 = new ArrayList<>();
+        val2.add("welcome to the class");
+        val1.addAll(val2);
+        System.out.println(val1);
+    }
+}
